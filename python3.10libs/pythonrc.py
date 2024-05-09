e36490906b19cc775107a827423e980abc599c08
yongsoon-vfx <yongg.soonn@gmail.com>
2024-05-09 12:24:50 +0800

    Added startup scripts for startup-time

--- python3.10libs/pythonrc.py
+++ python3.10libs/pythonrc.py
@@ -1,1 +1,4 @@
-print("Loaded YS_Tools")
+import os, time
+
+stime = round(time.time(), 2)
+os.environ["_stime"] = str(stime)
---
+++ python3.10libs/uiready.py
@@ -0,0 +1,6 @@
+import os, time
+
+stime = float(os.getenv("_stime"))
+ctime = round(time.time(), 2)
+time_taken = round(ctime - stime, 2)
+print(f"Time taken to load HDAs & GUI: {time_taken}s")
