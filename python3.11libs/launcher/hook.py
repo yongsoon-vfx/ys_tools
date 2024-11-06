f8b1de2cf272582eb91bbb85280eed304561eed0
yongsoon-vfx <yongg.soonn@gmail.com>
2024-11-06 23:47:09 +0800

    Refactor Node creation functionality to new class

--- python3.11libs/launcher/hook.py
+++ python3.11libs/launcher/hook.py
@@ -8,13 +8,36 @@ import hou
 from launcher.edit import *
 from launcher.keymap import *
 
+# Layout Constants
+VERTICAL_SPACING = [0, -1]
+
 
 class NodeEditorState(Enum):
-    NO_NODE_SELECTED = 0
+    NO_ITEM_SELECTED = 0
     LEAF_NODE_SELECTED = 1
     MIDDLE_NODE_SELECTED = 2
-    MULTIPLE_SELECTED = 3
-    INVALID_NODE_STATE = 4
+    CONNECTOR_SELECTED = 3
+    MULTIPLE_NODE_SELECTED = 4
+    MULTIPLE_CONNECTIONS_SELECTED = 5
+    INVALID_NODE_STATE = 6
+
+    @classmethod
+    def check_state() -> NodeEditorState:
+        selected_nodes = hou.selectedNodes()
+        selected_connections = hou.selectedConnections()
+
+        if len(selected_nodes) == 0 and len(selected_connections) == 0:
+            return NodeEditorState.NO_ITEM_SELECTED
+
+        if len(selected_nodes) > 0:
+            if len(selected_nodes[0].outputs()) == 0:
+                return NodeEditorState.LEAF_NODE_SELECTED
+
+            if len(selected_nodes[0].outputs()) >= 1:
+                return NodeEditorState.MIDDLE_NODE_SELECTED
+
+        if len(selected_connections) >= 1:
+            return NodeEditorState.CONNECTOR_SELECTED
 
 
 @dataclass
@@ -38,17 +61,71 @@ class NodeArranger:
                 self.getChildren(node_child)
 
 
-def check_state(selected_nodes: tuple[hou.Node]) -> NodeEditorState:
-    selected_nodes_length = len(selected_nodes)
-    if selected_nodes_length == 0:
-        return NodeEditorState.NO_NODE_SELECTED
-    if len(selected_nodes[0].outputs()) == 0:
-        return NodeEditorState.LEAF_NODE_SELECTED
-    return NodeEditorState.MIDDLE_NODE_SELECTED
+class NodeFactory:
+    def __init__(
+        self, node_type_name: str, network: hou.Node, state: NodeEditorState, **kwargs
+    ):
+        """
+        A class used to handle creating a hou.Node and arranging it
+        in the correct position in the tree
+
+        Args:
+            node_type_name (str): _description_
+            network (hou.Node): _description_
+            state (NodeEditorState): _description_
+            cursor_location (hou.Vector2,optional): _description_
+        """
+        self.created_node: hou.Node = network.createNode(node_type_name)
+
+        if state in [
+            NodeEditorState.LEAF_NODE_SELECTED,
+            NodeEditorState.MIDDLE_NODE_SELECTED,
+        ]:
+            self.selected_node: hou.Node = hou.selectedNodes()[-1]
+        self.state = state
+        self.cursor_location = kwargs.get("cursor_location", None)
+
+        self.createNode()
+        self.created_node.setGenericFlag(hou.nodeFlag.Display, True)
+        self.created_node.setGenericFlag(hou.nodeFlag.Render, True)
+
+    def createNode(self) -> int:
+        match self.state:
+            case NodeEditorState.NO_ITEM_SELECTED:
+                node_location = self.cursor_location
+                self.created_node.setPosition(node_location)
+                return 0
+
+            case NodeEditorState.LEAF_NODE_SELECTED:
+                self.created_node.setPosition(self.selected_node.position())
+                self.created_node.move(VERTICAL_SPACING)
+                self.created_node.setInput(0, self.selected_node)
+                return 0
+
+            case NodeEditorState.MIDDLE_NODE_SELECTED:
+                child_node: hou.Node = self.selected_node.outputs()[0]
+                input_connection_idx = 0
+                for i, node in enumerate(child_node.inputs()):
+                    print(node)
+                    if node == self.selected_node:
+                        input_connection_idx = i
+                print(f"input idx is {input_connection_idx}")
+                NA = NodeArranger(child_node)
+                NA.gatherChildren()
+                self.created_node.setPosition(self.selected_node.position())
+                self.created_node.move(VERTICAL_SPACING)
+                self.created_node.setInput(0, self.selected_node)
+                child_node.move(VERTICAL_SPACING)
+                child_node.setInput(input_connection_idx, self.created_node)
+                for node in NA.children_recursive:
+                    node.move(VERTICAL_SPACING)
+                return 0
+            case _:
+                raise "Invalid Node State"
 
 
 def on_key_press(event, state):
-    print(state)
+    # print(state)
     if event.char == "`":
         root.destroy()
         window_add_keymap()
@@ -80,38 +157,9 @@ def on_key_press(event, state):
     global network_node, cursor_loc
 
     try:
-        created_node: hou.Node = network_node.createNode(node)
-        created_node.setPosition(cursor_loc)
-        # Check State
-        if state == NodeEditorState.MIDDLE_NODE_SELECTED:
-            selected_node: hou.Node = hou.selectedNodes()[-1]
-
-            nodes_cache = NodeCache(
-                main_node=selected_node,
-                child_node=selected_node.outputs()[0],
-            )
-            NA = NodeArranger(nodes_cache.child_node)
-            NA.gatherChildren()
-            print(nodes_cache.child_node)
-            print(nodes_cache.main_node)
-            created_node.setPosition(nodes_cache.main_node.position())
-            created_node.move([0, -1])
-            created_node.setInput(0, nodes_cache.main_node, 0)
-            nodes_cache.child_node.move([0, -1])
-            nodes_cache.child_node.setInput(0, created_node, 0)
-            for output in NA.children_recursive:
-                output.move([0, -1])
-        if state == NodeEditorState.LEAF_NODE_SELECTED:
-            selected_node: hou.Node = hou.selectedNodes()[-1]
-            nodes_cache = NodeCache(
-                main_node=selected_node,
-                child_node=None,
-            )
-            print(nodes_cache.main_node)
-            created_node.setPosition(nodes_cache.main_node.position())
-            created_node.move([0, -1])
-            created_node.setInput(0, nodes_cache.main_node, 0)
-
+        created_node = NodeFactory(
+            node, network_node, state, cursor_location=cursor_loc
+        )
     except Exception as e:
         print(e)
         logging.error(f"Unable to create {node}. Perhaps check the name?")
@@ -124,7 +172,7 @@ def on_focus_out(_):
 
 
 def hook_window():
-    editor_state = check_state(hou.selectedNodes())
+    editor_state = check_state()
     global root
     # Create the main window
     root = tk.Tk()
