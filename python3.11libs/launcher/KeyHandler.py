948039236a9a89736f555a81aa46682794144993
yongsoon-vfx <yongg.soonn@gmail.com>
2024-11-11 01:26:09 +0800

    Major refactoring splitting hook.py. Key handling responsibilities, Execution and action responsibilities split into individual files and classes

---
+++ python3.11libs/launcher/KeyHandler.py
@@ -0,0 +1,36 @@
+import logging
+from pathlib import Path
+
+import tomllib as toml
+
+import tomli_w as tomlw
+from launcher.Executor import Action
+
+# Constants
+KEYMAP_FILE_NAME = "KeyMap.toml"
+KEYMAP_FILE_PATH = Path(__file__ + "/../" + KEYMAP_FILE_NAME)
+INDENT_SPACING = 4
+
+
+class KeymapReader:
+    def __init__(self):
+        # print(KEYMAP_FILE_PATH)
+        if not KEYMAP_FILE_PATH.exists():
+            print("Keymap Does not Exist")
+        self.key_map = {}
+        with open(KEYMAP_FILE_PATH, "rb") as f:
+            self.key_map = toml.load(f)
+
+    def ParseKey(self, network_context: str, key: str) -> Action:
+        return Action(self.key_map[network_context][key])
+
+
+class KeymapWriter:
+    def __init__(self):
+        print("end")
+
+
+# c = KeymapReader()
+# action = c.ParseKey("sop", "t")
+# action.Execute()
+# print(action.action)
--- python3.11libs/launcher/hook.py
+++ python3.11libs/launcher/hook.py
@@ -1,130 +1,12 @@
 import logging
 import tkinter as tk
-from dataclasses import dataclass
-from enum import Enum
-
-import hou
 
 from launcher.edit import *
+from launcher.KeyHandler import KeymapReader
 from launcher.keymap import *
 
-# Layout Constants
-VERTICAL_SPACING = [0, -1]
 
-
-class NodeEditorState(Enum):
-    NO_ITEM_SELECTED = 0
-    LEAF_NODE_SELECTED = 1
-    MIDDLE_NODE_SELECTED = 2
-    CONNECTOR_SELECTED = 3
-    MULTIPLE_NODE_SELECTED = 4
-    MULTIPLE_CONNECTIONS_SELECTED = 5
-    INVALID_NODE_STATE = 6
-
-    @classmethod
-    def check_state() -> NodeEditorState:
-        selected_nodes = hou.selectedNodes()
-        selected_connections = hou.selectedConnections()
-
-        if len(selected_nodes) == 0 and len(selected_connections) == 0:
-            return NodeEditorState.NO_ITEM_SELECTED
-
-        if len(selected_nodes) > 0:
-            if len(selected_nodes[0].outputs()) == 0:
-                return NodeEditorState.LEAF_NODE_SELECTED
-
-            if len(selected_nodes[0].outputs()) >= 1:
-                return NodeEditorState.MIDDLE_NODE_SELECTED
-
-        if len(selected_connections) >= 1:
-            return NodeEditorState.CONNECTOR_SELECTED
-
-
-@dataclass
-class NodeCache:
-    main_node: hou.Node
-    child_node: hou.Node
-
-
-class NodeArranger:
-    def __init__(self, root_node: hou.Node):
-        self.root_node = root_node
-        self.children_recursive = []
-
-    def gatherChildren(self):
-        self.getChildren(self.root_node)
-
-    def getChildren(self, node: hou.Node):
-        for node_child in node.outputs():
-            self.children_recursive.append(node_child)
-            if len(node_child.outputs()) > 0:
-                self.getChildren(node_child)
-
-
-class NodeFactory:
-    def __init__(
-        self, node_type_name: str, network: hou.Node, state: NodeEditorState, **kwargs
-    ):
-        """
-        A class used to handle creating a hou.Node and arranging it
-        in the correct position in the tree
-
-        Args:
-            node_type_name (str): _description_
-            network (hou.Node): _description_
-            state (NodeEditorState): _description_
-            cursor_location (hou.Vector2,optional): _description_
-        """
-        self.created_node: hou.Node = network.createNode(node_type_name)
-
-        if state in [
-            NodeEditorState.LEAF_NODE_SELECTED,
-            NodeEditorState.MIDDLE_NODE_SELECTED,
-        ]:
-            self.selected_node: hou.Node = hou.selectedNodes()[-1]
-        self.state = state
-        self.cursor_location = kwargs.get("cursor_location", None)
-
-        self.createNode()
-        self.created_node.setGenericFlag(hou.nodeFlag.Display, True)
-        self.created_node.setGenericFlag(hou.nodeFlag.Render, True)
-
-    def createNode(self) -> int:
-        match self.state:
-            case NodeEditorState.NO_ITEM_SELECTED:
-                node_location = self.cursor_location
-                self.created_node.setPosition(node_location)
-                return 0
-
-            case NodeEditorState.LEAF_NODE_SELECTED:
-                self.created_node.setPosition(self.selected_node.position())
-                self.created_node.move(VERTICAL_SPACING)
-                self.created_node.setInput(0, self.selected_node)
-                return 0
-
-            case NodeEditorState.MIDDLE_NODE_SELECTED:
-                child_node: hou.Node = self.selected_node.outputs()[0]
-                input_connection_idx = 0
-                for i, node in enumerate(child_node.inputs()):
-                    print(node)
-                    if node == self.selected_node:
-                        input_connection_idx = i
-                print(f"input idx is {input_connection_idx}")
-                NA = NodeArranger(child_node)
-                NA.gatherChildren()
-                self.created_node.setPosition(self.selected_node.position())
-                self.created_node.move(VERTICAL_SPACING)
-                self.created_node.setInput(0, self.selected_node)
-                child_node.move(VERTICAL_SPACING)
-                child_node.setInput(input_connection_idx, self.created_node)
-                for node in NA.children_recursive:
-                    node.move(VERTICAL_SPACING)
-                return 0
-            case _:
-                raise "Invalid Node State"
-
-
-def on_key_press(event, state):
+def on_key_press(event):
     # print(state)
     if event.char == "`":
         root.destroy()
@@ -140,29 +22,22 @@ def on_key_press(event, state):
     elif event.keysym == "Tab":
         import pprint
 
-        keymap = keymap_load()
+        keymap_reader = KeymapReader()
+        keymap = keymap_reader.key_map
         pprint.pp(keymap)
         root.destroy()
         return
 
     print(f"Key Pressed: {event.keysym}")
-    try:
-        node = keymap_lookup("sop", str(event.keysym))
-    except Exception as _:
-        print(f"{event.keysym} is not bound")
-        root.destroy()
-        return
-        # keymap_append("sop", "m", "merge")
-
-    global network_node, cursor_loc
 
     try:
-        created_node = NodeFactory(
-            node, network_node, state, cursor_location=cursor_loc
-        )
+        s = KeymapReader()
+        print(s.key_map)
+        action = s.ParseKey("sop", str(event.keysym))
+        action.Execute()
     except Exception as e:
         print(e)
-        logging.error(f"Unable to create {node}. Perhaps check the name?")
+        logging.error(f"Unable to create. Perhaps check the name?")
     root.destroy()
 
 
@@ -172,7 +47,6 @@ def on_focus_out(_):
 
 
 def hook_window():
-    editor_state = check_state()
     global root
     # Create the main window
     root = tk.Tk()
@@ -188,20 +62,9 @@ def hook_window():
     # Focus on the window
     root.focus_force()
     # Bind the key press event
-    root.bind("<KeyPress>", lambda event: on_key_press(event, editor_state))
+    root.bind("<KeyPress>", lambda event: on_key_press(event))
     root.bind("<FocusOut>", on_focus_out)
     root.mainloop()
 
 
-def get_network_tab_location():
-    network_tab = hou.ui.paneTabUnderCursor()
-    node = network_tab.pwd()
-    cursor_loc = network_tab.cursorPosition()
-    logging.debug(f"Cursor Location:{cursor_loc}")
-    logging.debug(f"Node Path:{node}")
-
-    return cursor_loc, node
-
-
-cursor_loc, network_node = get_network_tab_location()
 hook_window()
